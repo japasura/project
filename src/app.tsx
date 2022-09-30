bb02a799da0b01ae66d95c56ffed63abc6d9198b
Samarth <samarthr1@outlook.com>
2022-09-30 20:45:32 +0530

    REFACTOR: move the control for the time selection to its own component

--- src/app.tsx
+++ src/app.tsx
@@ -1,100 +1,95 @@
 import {ReactNode} from "react";
 
-const TableItem = (props: {children?: ReactNode}) => {
+const TableItem = (props: { children?: ReactNode }) => {
     return <td className={"border w-20 text-center"}>
         {props.children}
     </td>
 }
 
-function HeadingRow(){
+function HeadingRow() {
     const nums = ['00-15', '15-30', '30-45', '45-00']
 
     return <tr className={"border"}>
         <TableItem/>
-        {
-            nums.map((k, v) => {
-                return <TableItem key={v}>{k}</TableItem>
-            })
-        }
+        {nums.map((k, v) => {
+            return <TableItem key={v}>{k}</TableItem>
+        })}
         <TableItem/>
         <TableItem/>
-        {
-            nums.map((k, v) => {
-                return <TableItem key={v}>{k}</TableItem>
-            })
-        }
+        {nums.map((k, v) => {
+            return <TableItem key={v}>{k}</TableItem>
+        })}
     </tr>
 }
 
-function TimeTrackerBody(){
+function TimeTrackerBody() {
     const hours = Array.from(Array(12).keys())
     const arr = Array.from(Array(4).keys());
     return <>
-        {
-            hours.map((h, i) => {
-                return <tr key={i}>
-                    <TableItem>{h}</TableItem>
-                    {
-                        arr.map(i => {
-                            return <TableItem key={i}/>
-                        })
-                    }
-                    <TableItem/>
-                    <TableItem>{h+12}</TableItem>
-                    {
-                        arr.map(i => {
-                            return <TableItem key={i}/>
-                        })
-                    }
-                </tr>
-            })
-        }
+        {hours.map((h, i) => {
+            return <tr key={i}>
+                <TableItem>{h}</TableItem>
+                {arr.map(i => {
+                    return <TableItem key={i}/>
+                })}
+                <TableItem/>
+                <TableItem>{h + 12}</TableItem>
+                {arr.map(i => {
+                    return <TableItem key={i}/>
+                })}
+            </tr>
+        })}
     </>
 }
 
-
-function TimeTrackerControls(){
+function TimeSelector() {
     const hours = Array.from(Array(24).keys())
     const minutes = ['00-15', '15-30', '30-45', '45-00']
 
-    return <div className={"flex flex-row justify-evenly w-full"}>
-            <span>
-                <input className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" +
-                    " focus-visible:border-gray-800"} placeholder={"Task"} />
-            </span>
-        <div className={"flex flex-row gap-3"}>
-            <div className={"flex gap-1 flex-row items-center"}>
-                <label>Hour</label>
-                <select className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" +
-                    " focus-visible:border-gray-800"}>
-                    {
-                        hours.map(h => {
-                            return <option key={h} value={h}>{h}</option>
-                        })
-                    }
-                </select>
-            </div>
-            <div className={"flex gap-1 flex-row items-center"}>
-                <label>Minutes</label>
-                <select className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" +
-                    " focus-visible:border-gray-800"}>
-                    {
-                        minutes.map(h => {
-                            return <option key={h} value={h}>{h}</option>
-                        })
-                    }
-                </select>
-            </div>
+    return <div className={"flex flex-row gap-3"}>
+        <div className={"flex gap-1 flex-row items-center"}>
+            <label>Hour</label>
+            <select
+                className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" + " focus-visible:border-gray-800"}>
+                {hours.map(h => {
+                    return <option key={h} value={h}>{h}</option>
+                })}
+            </select>
         </div>
-        <span>
-                <select value={0} className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" +
-                    " focus-visible:border-gray-800"}>
+        <div className={"flex gap-1 flex-row items-center"}>
+            <label>Minutes</label>
+            <select
+                className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" + " focus-visible:border-gray-800"}>
+                {minutes.map(h => {
+                    return <option key={h} value={h}>{h}</option>
+                })}
+            </select>
+        </div>
+    </div>
+}
+
+function TimeTrackerControls() {
+
+    return <>
+        <div className={"flex flex-row justify-evenly w-full"}>
+            <span>
+                <input
+                    className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" +
+                        " focus-visible:border-gray-800"}
+                    placeholder={"Task"}/>
+            </span>
+            <TimeSelector/>
+            <span>
+                <select value={0}
+                        className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" +
+                            " focus-visible:border-gray-800"}>
                     <option value={0} disabled={true}>Color</option>
                     <option>Red</option>
                 </select>
             </span>
 
-    </div>
+        </div>
+    </>
 }
 
 export default function App() {
@@ -103,10 +98,10 @@ export default function App() {
         <div>
             <table className={"table border"}>
                 <thead>
-                    <HeadingRow/>
+                <HeadingRow/>
                 </thead>
                 <tbody>
-                    <TimeTrackerBody/>
+                <TimeTrackerBody/>
                 </tbody>
             </table>
         </div>
