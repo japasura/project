ab3dde89cd051356e20e08e3b98c4eb56bbc17bb
Sai Sudhir <saisudhirs@outlook.com>
2022-11-16 19:09:44 +0530

    WIP: Scheduler renamed to TimeSheet. Event state added.

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,12 @@
 import TimeSheet from "./Scheduler";
 import Grid from "@mui/material/Grid";
 import TimeTrackerControls from "./Controls";
+import {useState} from "react";
 
 
 
 export default function App() {
+    const [event, setEvent] = useState({})
     return <Grid container direction={"row"} gap={3}>
         {/*<h1 className={"text-3xl font-bold"}>Clocker</h1>*/}
         <Grid sm={8}>
