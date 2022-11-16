8914b45029720743cfa1f7df7f10d7bf5d7d40b9
Sai Sudhir <saisudhirs@outlook.com>
2022-11-16 19:03:34 +0530

    WIP: Front end

---
+++ src/Controls.tsx
@@ -0,0 +1,41 @@
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import DateTimeSelection from "./DateTimeSelection";
+
+export default function TimeTrackerControls() {
+
+    const submit = () => {
+        alert("Submitting!")
+    }
+
+    const cancel = () => {
+        alert("Clearing")
+    }
+
+    return <Grid container direction={"column"} gap={2} padding={2} alignContent={"center"}>
+        <Grid>
+            <TextField label={"Task"}/></Grid>
+        {/*<TimeSelector/>*/}
+        <Grid>
+            <select value={0}
+                    className={"p-3 bg-inherit border border-gray-800 rounded focus-visible:border" + " focus-visible:border-gray-800"}>
+                <option value={0} disabled={true}>Color</option>
+                <option>Red</option>
+            </select>
+        </Grid>
+        <Grid>
+            <DateTimeSelection label={"Start"}/>
+        </Grid>
+        <Grid>
+            <DateTimeSelection label={"End"}/>
+        </Grid>
+        <Grid container gap={1}>
+            <button className={"border p-3 border-gray-200 shadow text-blue-600 uppercase text-sm rounded"}
+                    onClick={submit}>Save
+            </button>
+            <button className={"border p-3 border-gray-200 shadow text-red-600 uppercase text-sm rounded"}
+                    onClick={cancel}>Cancel
+            </button>
+        </Grid>
+    </Grid>
+}
--- src/Scheduler.tsx
+++ src/Scheduler.tsx
@@ -1,18 +1,65 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
-import {ViewState} from '@devexpress/dx-react-scheduler';
+import {IntegratedEditing, ViewState} from '@devexpress/dx-react-scheduler';
 import {Appointments, DayView, Scheduler,} from '@devexpress/dx-react-scheduler-material-ui';
 import useEvents from "./hooks/useEvents";
 
 
 export default () => {
-
     const [schedulerData] = useEvents()
     return <Paper sx={{p: 2}}>
         <Scheduler data={schedulerData}>
             <ViewState currentDate={'2022-11-16'}/>
-            <DayView startDayHour={6} endDayHour={24}/>
+            <IntegratedEditing />
+            <DayView startDayHour={0} endDayHour={24}/>
             <Appointments/>
         </Scheduler>
     </Paper>
 };
+//
+//
+// import * as React from 'react';
+// import Paper from '@mui/material/Paper';
+// import { ViewState } from '@devexpress/dx-react-scheduler';
+// import {
+//     Scheduler,
+//     MonthView,
+//     Toolbar,
+//     DateNavigator,
+//     Appointments,
+//     TodayButton,
+// } from '@devexpress/dx-react-scheduler-material-ui';
+//
+// import { appointments } from '../../../demo-data/month-appointments';
+// import useEvents from "./hooks/useEvents";
+//
+// export default class Demo extends React.PureComponent {
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             data: = useEvents()
+//         };
+//     }
+//
+//     render() {
+//         const { data } = this.state;
+//
+//         return (
+//             <Paper>
+//                 <Scheduler
+//                     data={data}
+//                 >
+//                     <ViewState
+//                         defaultCurrentDate="2018-07-27"
+//                     />
+//                     <MonthView />
+//                     <Toolbar />
+//                     <DateNavigator />
+//                     <TodayButton />
+//                     <Appointments />
+//                 </Scheduler>
+//             </Paper>
+//         );
+//     }
+// }
