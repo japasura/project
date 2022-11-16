112235ad9a0fc3b9cd7a5b37450dff34b404010e
Samarth <samarthr1@outlook.com>
2022-11-16 17:41:56 +0530

    FEATURE: start getting the details from the server

--- src/Scheduler.tsx
+++ src/Scheduler.tsx
@@ -1,36 +1,18 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import {
-    Scheduler,
-    DayView,
-    Appointments,
-} from '@devexpress/dx-react-scheduler-material-ui';
+import {ViewState} from '@devexpress/dx-react-scheduler';
+import {Appointments, DayView, Scheduler,} from '@devexpress/dx-react-scheduler-material-ui';
+import useEvents from "./hooks/useEvents";
 
-const currentDate = '2018-11-01';
-const schedulerData = [
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-    { startDate: '2018-11-01T14:00', endDate: '2018-11-01T14:30', title: 'Go to a gym2' },
-    { startDate: '2018-11-01T15:00', endDate: '2018-11-01T15:30', title: 'Go to a gym3' },
-    { startDate: '2018-11-01T17:20', endDate: '2018-11-01T17:30', title: 'Go to a gym4' },
-    { startDate: '2018-11-01T19:50', endDate: '2018-11-01T21:30', title: 'Go to a gym5' },
 
-];
+export default () => {
 
-export default () => (
-    <Paper sx={{p: 2}}>
-        <Scheduler
-            data={schedulerData}
-        >
-            <ViewState
-                currentDate={currentDate}
-            />
-            <DayView
-                startDayHour={9}
-                endDayHour={22}
-            />
-            <Appointments />
+    const [schedulerData] = useEvents()
+    return <Paper sx={{p: 2}}>
+        <Scheduler data={schedulerData}>
+            <ViewState currentDate={'2022-11-16'}/>
+            <DayView startDayHour={6} endDayHour={24}/>
+            <Appointments/>
         </Scheduler>
     </Paper>
-);
+};
---
+++ src/hooks/useEvents.ts
@@ -0,0 +1,41 @@
+import {useEffect, useState} from "react";
+
+export interface UserEvents {
+    startDate: string,
+    endDate: string,
+    title: string
+}
+
+interface ApiUserEvents {
+    start_time: number,
+    end_time: number,
+    type: string
+}
+
+export default function useEvents(){
+    const [events, setEvents] = useState<Array<UserEvents>>([])
+    const date = new Date()
+    const d_str = `${date.getFullYear()}-${date.getMonth() +1}-${11}`
+    console.log()
+    useEffect(()=> {
+        fetch("http://122.166.189.206/dayData/" + d_str + "/events", {headers: {"Api-Key": "hi"}}).then(resp => {
+            if (resp.ok){
+                resp.json().then((j: Array<ApiUserEvents>) => {
+                    const sv: Array<UserEvents> = j.map(s => {
+                        const start = new Date(s.start_time * 1000)
+                        const end = new Date(s.end_time * 1000)
+                        return {
+                            startDate:start.toISOString(),
+                            endDate: end.toISOString(),
+                            title: s.type
+                        }
+                    })
+                    console.log(sv)
+                    setEvents(sv)
+                })
+            }
+
+        })
+    }, [])
+    return [events]
+}
