6f47511f91038a8e8e8ba5204a49cec78c5a2c2c
Samarth <samarthr1@outlook.com>
2022-11-17 13:52:07 +0530

    FEATURE: improve messags, redirect on sign in

--- src/login.tsx
+++ src/login.tsx
@@ -5,12 +5,14 @@ import {Button, Box} from "@mui/material"
 import {useState} from "react";
 import useLogin from "./hooks/useLogin";
 import useCreateAccount from "./hooks/useCreateAccount";
+import {useNavigate} from "react-router-dom";
 
 export default function Login(props: {authToken: string}) {
     const [state_1, setState_1] = useState({username: "", password: ""})
     const [state_2, setState_2] = useState({username: "", password: ""})
     const login = useLogin(props.authToken)
     const createaccount = useCreateAccount(props.authToken)
+    const navigate = useNavigate()
 
     const onChange_1 = (e: React.ChangeEvent<HTMLInputElement>) => {
         setState_1({...state_1, [e.target.name]: e.target.value})
@@ -19,15 +21,20 @@ export default function Login(props: {authToken: string}) {
     const onChange_2 = (e: React.ChangeEvent<HTMLInputElement>) => {
         setState_2({...state_2, [e.target.name]: e.target.value})
     }
+
     const onSubmit_1 = () => {
         login(state_1.username, state_1.password).then(r=> {
-            alert(r)
+            if (r){
+                navigate("/dash")
+            }
         })
     }
 
     const onSubmit_2 = () => {
         createaccount(state_2.username, state_2.password).then(r=> {
-            alert(r)
+            if (r){
+                alert("Sign In with your new credentials")
+            }
         })
     }
 
