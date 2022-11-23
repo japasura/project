43f259914275fc3187b59d7c7e464745641d203f
Samarth <samarthr1@outlook.com>
2022-11-23 11:12:59 +0530

    FEATURE: add in useAnalytics.ts

---
+++ src/hooks/useAnalytics.ts
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+import {host} from "../index";
+import {getDateString} from "./useEvents";
+
+export interface AnalyticsData {
+    start_date: string,
+    end_date: string,
+    analytics: any
+}
+
+export default function useAnalytics(authToken: string, startDate: string) {
+    const [details, setDetails] = useState<AnalyticsData>()
+
+    useEffect(() => {
+        fetch(host + "/analytics", {
+            method: "POST",
+            body: JSON.stringify({
+                start_date: startDate,
+                end_date: getDateString(new Date())
+            })
+        }).then(r => {
+            r.json().then(data => {
+                setDetails(data)
+            })
+        })
+    }, [authToken, startDate])
+
+    return details
+}
